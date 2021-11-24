1cb708d16254bfd0cfaca6b49bb47e21ae6dde77
Vlad Nikolov <vlad@viapontica.ai>
2021-11-24 21:08:50 +0200

    TS fix

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ declare function useImage(
   url: string,
   options?: {
     mode?: "no-cors" | "cors" | "same-origin";
-    headers?: Headers;
+    headers?: any;
     credentials?: "include" | "same-origin" | "omit";
     cache?: "default" | "no-cache" | "reload" | "force-cache" | "only-if-cached";
   }
